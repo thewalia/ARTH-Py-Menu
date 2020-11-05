ca1d9c5e03921d64cc1e6b72af1df1de1c8db085
thewalia <65010347+thewalia@users.noreply.github.com>
2020-11-05 15:55:54 +0530

    Version 1.1

--- menu.py
+++ menu.py
@@ -45,20 +45,31 @@ import os
 rows, columns = os.popen('stty size', 'r').read().split()
 width = int(columns)
 
+os.system('clear')
+
 hashLine = '#'
 print(colors.fg.pink,end="")
 print(hashLine.center(width,'#'),colors.reset)
-print('\n\n\n\n')
+print('\n\n\n\n\n\n')
 
 print(colors.fg.red, colors.bold,'\t\t\t\t\t\t\t\t\t\tA',end="")
 print(colors.reset,end="")
 print("rth\n\n")
 print("\t\t\t\t\t\t\t\t\t     Team Task\n\n")
-print(colors.fg.lightcyan,"\t\t\t\t\t   'Menu based Python program integrating all learnt technologies'",colors.reset)
-print("\nWelcome!!\n")
+print(colors.fg.lightcyan,"\t\t\t\t\t\t   'Menu based Python program integrating all learnt technologies'",colors.reset)
+print("\n\n")
+
+welcomeLine = 'Welcome!!'
+print(welcomeLine.center(width))
+
 print(colors.fg.lightgrey,end="")
-continueLine = "Press any key to Continue..."
+continueLine = "Press enter to Continue..."
 print(continueLine.center(width),colors.reset)
+
+print('\n\n\n\n\n\n')
+print(colors.fg.pink,end="")
+print(hashLine.center(width,'#'),colors.reset)
+input()
 
 ####################################
 ############Displaying Menu
@@ -68,5 +79,71 @@ os.system('clear')
 hashLine = '#'
 print(colors.fg.pink,end="")
 print(hashLine.center(width,'#'),colors.reset)
-printMenu = "\nMenu Items :"
+print("\n")
+printMenu = "Menu Items :"
 print(printMenu.center(width))
+
+print(colors.fg.yellow)
+print("Press 1: To see date")
+print(colors.fg.purple)
+print("Press 2: To see calendar")
+print(colors.fg.yellow)
+print("Press 3: To set-up Hadoop Cluster")
+print(colors.fg.purple)
+print("Press 4: To create new AWS EC-2 instance")
+print(colors.fg.yellow)
+print("Press 5: To delete AWS EC-2 instance")
+print(colors.fg.purple)
+print("Press 6: To create new AWS S3 bucket")
+print(colors.fg.yellow)
+print("Press 7: To delete AWS S3 bucket")
+print(colors.fg.purple)
+print(colors.reset)
+
+choice = input("Enter your choice: ")
+
+if choice == '1' :
+    os.system('clear')
+    os.system('date')
+elif choice == '2' :
+    os.system('clear')
+    os.system('cal')
+elif choice == '3' :
+    os.system('clear')
+    print("Press A: To continue for Local Cluster")
+    print("Press B: To continue for Cloud Cluster")
+    choice2 = input("Enter your choice: ")
+    if choice2 == 'A' :
+        os.system('clear')
+        print("#####Working in Local Cluster#####\n\n")
+        # Code
+    elif choice2 == 'B':
+        os.system('clear')
+        print("#####Working in Remote Cluster#####\n\n")
+        # Code
+    else : 
+        print(colors.fg.red,"Wrong Input! Pls try again.",colors.reset)
+elif choice == '4' :
+    os.system('clear')
+    print("Creating new AWS EC-2 instance\n\n\n")
+    ##################
+    ########## code
+    ##################
+elif choice == '5' :
+    os.system('clear')
+    print("Deleting AWS EC-2 instance\n\n\n")
+    ##################
+    ########## code
+    ##################
+elif choice == '6' :
+    os.system('clear')
+    print("Creating new AWS S3 bucket\n\n\n")
+    ##################
+    ########## code
+    ##################
+elif choice == '7':
+    os.system('clear')
+    print("Deleting AWS S3 bucket\n\n\n")
+    ##################
+    ########## code
+    ##################
